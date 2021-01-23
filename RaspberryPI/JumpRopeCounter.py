c20067270a14bb28d9bada0a1d0202d60aa43df7
Jo Creyf <jo.creyf@gmail.com>
2021-01-22 20:19:33 -0800

    Adding GUI parts

--- RaspberryPI/JumpRopeCounter.py
+++ RaspberryPI/JumpRopeCounter.py
@@ -17,14 +17,13 @@ port_sounddetector = 17
 sounddetector_bouncetime = 200
 
 
-#
-# Callback method that will get called each time the SoundDetector triggers:
-#
 def callbackSoundDetector(port_sounddetector):
-  global counter
+  """
+  Callback method that will get called each time the SoundDetector triggers
+  """
+  global counter, lbl
   counter += 1
   print(f'{counter:5}')
-#  lblText.set(f'{counter:5}')
   lbl.config(text=f'{counter:5}')
   lbl.update_idletasks()
 
@@ -47,12 +46,9 @@ GPIO.add_event_callback(port_sounddetector, callbackSoundDetector)
 window = Tkinter.Tk()
 window.title("JumpRope Counter")
 window.geometry('350x200')
-#lblText = StringVar()
-#lbl = Tkinter.Label(window, textvariable=lblText, font=("Arial Bold", 50)).pack()
 lbl = Tkinter.Label(window, text=f'{counter:5}', font=("Arial Bold", 50))
 lbl.grid(column=0, row=0)
 lbl.pack()
-#lblText.set(f'{counter:5}')
 
 # Now loop:
 window.mainloop()
