b986929bdafa9a1235e0b7de9e66eca7074e609d
Jo Creyf <jo.creyf@gmail.com>
2021-01-22 19:43:46 -0800

    Adding a counter

--- RaspberryPI/JumpRopeCounter.py
+++ RaspberryPI/JumpRopeCounter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #***************************************************************#
 # Application to count the number of rope jumps.                #
 # This is using the DAOKI Sound Sensor to try detect the sound  #
@@ -11,21 +11,25 @@
 import RPi.GPIO as GPIO
 import time
 
-# Setting up the GPIO pins:
+counter = 0
 port_sounddetector = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(port_sounddetector, GPIO.IN)
+
 
 #
 # Callback method that will get called each time the SoundDetector port goes either
 # high or low:
 #
 def callbackSoundDetector(port_sounddetector):
+  counter += 1
   if GPIO.input(port_sounddetector):
-    print "Sound Detected! (hi)"
+    print(f'H:{counter:5}')
   else:
-    print "Sound Detected! (low)"
+    print(f'H:{counter:5}')
 
+
+# Setting up the GPIO pins:
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(port_sounddetector, GPIO.IN)
 
 # Set a filter for an event trigger on the SoundDetector port.
 # We want to get an event on both the high and low values of the port, using
