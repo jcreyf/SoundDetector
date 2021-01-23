0264c986929452efb2697189fc3099c1f1791267
jcreyf <jo.creyf@gmail.com>
2021-01-22 17:41:27 -0800

    Begin this new project

---
+++ JumpRopeCounter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#***************************************************************#
+# Application to count the number of rope jumps.                #
+# This is using the DAOKI Sound Sensor to try detect the sound  #
+# of the rope hitting the ground and use that to increment a    #
+# counter on screen.                                            #
+#---------------------------------------------------------------#
+# 2021-01-22  jcreyf  v0.1  Initial version                     #
+#***************************************************************#
+
+import RPi.GPIO as GPIO
+import time
+
+# Setting up the GPIO pins:
+port_sounddetector = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(port_sounddetector, GPIO.IN)
+
+#
+# Callback method that will get called each time the SoundDetector port goes either
+# high or low:
+#
+def callbackSoundDetector(port_sounddetector):
+  if GPIO.input(port_sounddetector):
+    print "Sound Detected!"
+  else:
+    print "Sound Detected!"
+
+
+# Set a filter for an event trigger on the SoundDetector port.
+# We want to get an event on both the high and low values of the port, using
+# a debounce period of 300ms:
+GPIO.add_event_detect(port_sounddetector, GPIO.BOTH, bouncetime=300)
+# Attach our callback method:
+GPIO.add_event_callback(port_sounddetector, callbackSoundDetector)
+
+# Now loop:
+while True:
+  time.sleep(1)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SoundDetector
-Projects using the Daoki Sound Sensor
+Projects using the Daoki Sound Sensor:
+![_documentation/resources/DAOKI_SoundSensor.png]
+
+Ideas and initial code taken from: [Piddler In The Root](http://www.piddlerintheroot.com/sound-sensor/)
+
